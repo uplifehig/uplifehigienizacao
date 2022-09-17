046c40e91ef3084d9dc97dfaf25a1bd33289aa96
uplifehig <brunochimenes_@hotmail.com>
2022-09-17 19:33:20 -0300

    ajuste script

--- js/modulo/initMenu.js
+++ js/modulo/initMenu.js
@@ -2,7 +2,6 @@ export default function initMenu() {
   const menu = document.querySelector("[data-menu]");
   const botao = document.querySelector(".menu");
 
-  console.log(botao, menu);
   function botaoMenu() {
     menu.classList.toggle(menu.dataset.menu);
   }
